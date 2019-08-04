9053ce89f640fb2719091559c964bbd02e767eaf
Khalil EL ISMAILI <khalil_simo@hotmail.fr>
2019-08-04 11:06:17 +0200

    Added Readux Logger

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,30 +1,36 @@
 import createSagaMiddleware from "redux-saga";
 import { applyMiddleware, compose, createStore } from "redux";
 import { createBrowserHistory } from 'history';
-import { reducers } from "../reducers/index";
 import { routerMiddleware, syncHistoryWithStore } from "react-router-redux";
-import { sagas } from "../sagas/index";
 
-// add the middlewares
-let middlewares = [];
-
-// add the router middleware
-middlewares.push(routerMiddleware(createBrowserHistory()));
+import { reducers } from "../reducers";
+import sagas from "../sagas";
 
 // add the saga middleware
 const sagaMiddleware = createSagaMiddleware();
-middlewares.push(sagaMiddleware);
 
-// apply the middleware
-let middleware = applyMiddleware(...middlewares);
+// add the middlewares
+let middlewares = [
+  sagaMiddleware,
+  routerMiddleware(createBrowserHistory()),
+];
 
-// add the redux dev tools
-if (process.env.NODE_ENV !== 'production' && window.devToolsExtension) {
-  middleware = compose(middleware, window.devToolsExtension());
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
 }
 
+// add the redux dev tools
+/* if (process.env.NODE_ENV !== 'production' && window.devToolsExtension) {
+  middleware = compose(middleware, window.devToolsExtension());
+} */
+
 // create the store
-const store = createStore(reducers, middleware);
+const store = createStore(
+  reducers,
+  compose(applyMiddleware(...middlewares))
+);
+
 const history = syncHistoryWithStore(createBrowserHistory(), store);
 sagaMiddleware.run(sagas);
 
