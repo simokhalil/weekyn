e80797ec0b637e299877836a434f8de5347a51e7
Khalil EL ISMAILI <khalil_simo@hotmail.fr>
2019-08-05 23:50:42 +0200

    Fixed firebase conf

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const prodConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  databaseURL: 'YOUR_DATABASE_URL',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: '',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  apiKey: 'AIzaSyDCRwDpaLBe5vUifXNsLQpND-6Tj1MihDA',
+  authDomain: 'weekyn-fea79.firebaseapp.com',
+  databaseURL: 'https://weekyn-fea79.firebaseio.com',
+  projectId: 'weekyn-fea79',
+  storageBucket: 'weekyn-fea79.appspot.com',
+  messagingSenderId: '617757654254',
 };
 
 const devConfig = {
